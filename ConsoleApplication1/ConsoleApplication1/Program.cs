4ba9dcba2a8801a3fe15afed884a32da59546b35
munozc <tecktomikflo@gmail.com>
2014-09-23 08:44:38 -0400

    int list

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = new int[5] { 10, 20, 30, 40, 50 };
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+                Console.WriteLine(ints[i]);
+            }
+
+            for (int i = ints.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(ints[i]);
+            }
+        }
+    }
+}
